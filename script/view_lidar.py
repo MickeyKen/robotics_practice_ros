41180ad774b2b49484a9da27128d9bbd95cafe43
MickeyKen <mickey333ff@gmail.com>
2019-04-06 19:46:57 +0900

    [Add] plot program

--- script/view_lidar.py
+++ script/view_lidar.py
@@ -3,8 +3,12 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
-# メッセージの型等のimport
 from geometry_msgs.msg import Pose2D
+
+# for plot
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Publishsers():
     def make_msg(self, x, y, theta):
@@ -16,19 +20,24 @@ class Publishsers():
     def send_msg(self):
         self.publisher.publish(self.message)
 
+
 class Subscribe_publishers(Publishsers):
     def __init__(self):
 
         self.subscriber = rospy.Subscriber('/scan', LaserScan, self.callback)
         self.publisher = rospy.Publisher('/calcurated_pose', Pose2D, queue_size=10)
-
         self.message = Pose2D()
+
     def callback(self, msg):
 
         print msg.ranges[0]
-
+        x = np.random.randn(30)
+        y = np.sin(x) + np.random.randn(30)
+        plt.plot(msg.ranges[0], msg.ranges[1], "o")
+        plt.axis("equal")
+        plt.draw()
+        plt.pause(0.00000000001)
         self.make_msg(0.0,0.0,0.0)
-
         self.send_msg()
 
 def main():
@@ -36,8 +45,9 @@ def main():
     rospy.init_node('view_lidar')
 
     sub = Subscribe_publishers()
-
+    plt.ion()
+    plt.show()
     rospy.spin()
 
 if __name__ == '__main__':
-   main()
+    main()
