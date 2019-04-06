b49ff7c092a97541c84b7951265fdd11cbab2768
MickeyKen <mickey333ff@gmail.com>
2019-04-06 19:01:03 +0900

    [Add] basic sub_pub program

---
+++ script/view_lidar.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from sensor_msgs.msg import LaserScan
+# メッセージの型等のimport
+from geometry_msgs.msg import Pose2D
+
+class Publishsers():
+    def make_msg(self, x, y, theta):
+
+       self.message.x = x
+       self.message.y = y
+       self.message.theta = theta
+
+    def send_msg(self):
+        self.publisher.publish(self.message)
+
+class Subscribe_publishers(Publishsers):
+    def __init__(self):
+
+        self.subscriber = rospy.Subscriber('/scan', LaserScan, self.callback)
+        self.publisher = rospy.Publisher('/calcurated_pose', Pose2D, queue_size=10)
+
+        self.message = Pose2D()
+    def callback(self, msg):
+
+        print msg.ranges[0]
+
+        self.make_msg(0.0,0.0,0.0)
+
+        self.send_msg()
+
+def main():
+
+    rospy.init_node('view_lidar')
+
+    sub = Subscribe_publishers()
+
+    rospy.spin()
+
+if __name__ == '__main__':
+   main()
