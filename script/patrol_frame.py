e748a4d463116276ee98efd95152cd0b8a450c93
MickeyKen <mickey333ff@gmail.com>
2019-11-22 11:19:41 +0900

    [Fix] goal server

--- script/patrol_frame.py
+++ script/patrol_frame.py
@@ -6,7 +6,7 @@ import math
 import os
 import rospkg
 
-from ubiquitous_display_msgs.srv import PatrolCommand
+from ubiquitous_display_msgs.srv import PatrolCommand, PatrolCommandRequest
 from ubiquitous_display_msgs.srv import PatrolCommandResponse
 
 LOOP_NUM = 2
@@ -15,7 +15,9 @@ LOOP_NUM *= POINT_NUM
 
 rospy.init_node('patrol_main_server')
 
-goal_pos = np.zeros((POINT_NUM, 2))
+pat_ser = rospy.ServiceProxy('/patrol_server', PatrolCommand)
+
+goal_pos = np.zeros((POINT_NUM, 2), dtype = 'float64')
 
 file_name = "goal_points"
 path = os.path.join(rospkg.RosPack().get_path('robotics_practice')+ '/txt', file_name)
@@ -26,16 +28,16 @@ count = 0
 for texts in file_content:
     text = texts.replace('\n'," ")
     text = texts.split(" ")
-    goal_pos[count][0] = text[0]
-    goal_pos[count][1] = text[1]
+    goal_pos[count][0] = float(text[0])
+    goal_pos[count][1] = float(text[1])
     count += 1
 
 for loop in range(LOOP_NUM):
     x = float(goal_pos[loop % POINT_NUM][0])
     y = float(goal_pos[loop % POINT_NUM][1])
-    # req = PatrolCommandRequest()
-    # req.goal_position.position.x = x
-    # req.goal_position.position.y = y
-    # result = self.nav(req)
-    # self.result = result.result.data
-    print x, y
+    req = PatrolCommandRequest()
+    req.goal_position.position.x = x
+    req.goal_position.position.y = y
+    result = pat_ser(req)
+    result = result.result.data
+    print x, y, result
--- script/patrol_main_server.py
+++ script/patrol_main_server.py
@@ -9,9 +9,9 @@ from std_msgs.msg import String, Bool
 from visualization_msgs.msg import Marker, MarkerArray
 
 from actionlib_msgs.msg import GoalStatusArray
-
-from pir2_msgs.srv import PatrolCommand
-from pir2_msgs.srv import PatrolCommandResponse
+from move_base_msgs.msg import MoveBaseActionResult
+from ubiquitous_display_msgs.srv import PatrolCommand
+from ubiquitous_display_msgs.srv import PatrolCommandResponse
 
 
 class Publishers():
@@ -21,7 +21,7 @@ class Publishers():
         goal_msg.pose.position.x = x
         goal_msg.pose.position.y = y
         goal_msg.pose.orientation.w = 1.0
-        goal_msg.header.frame_id = "base_footprint"
+        goal_msg.header.frame_id = "map"
         self.goal_pub.publish(goal_msg)
 
     def make_frag_pub(self, x, y):
@@ -31,8 +31,8 @@ class Publishers():
        marker.header.frame_id = "/map"
        marker.type = marker.SPHERE
        marker.action = marker.ADD
-       marker.scale.x = 0.2
-       marker.scale.y = 0.2
+       marker.scale.x = 1.0
+       marker.scale.y = 1.0
        marker.scale.z = 0.0
        marker.color.a = 1.0
        marker.color.r = 1.0
@@ -41,9 +41,11 @@ class Publishers():
        marker.pose.orientation.w = 1.0
        marker.pose.position.x = x
        marker.pose.position.y = y
-       marker.pose.position.z = 0.0
+       marker.pose.position.z = 3.0
+       # marker.lifetime = -1
 
        markerArray.markers.append(marker)
+       self.goal_frag_pub.publish(markerArray)
 
 
 class Subscribe(Publishers):
@@ -56,30 +58,48 @@ class Subscribe(Publishers):
         self.goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=100)
 
         # Declaration Subscriber
-        self.nav_sub = rospy.Subscriber('/move_base/status', GoalStatusArray, self.nav_callback)
-
+        # self.nav_sub = rospy.Subscriber('/move_base/status', GoalStatusArray, self.nav_callback)
+        self.nav_sub = rospy.Subscriber('/move_base/result', MoveBaseActionResult, self.nav2_callback)
         # Declaration Service Server
-        self.server = rospy.Service("/patrol_server", NavCommand, self.service_callback)
+        self.server = rospy.Service("/patrol_server", PatrolCommand, self.service_callback)
 
     ### callback function for amcl node (pose) ###
-    def nav_callback(self, msg):
-        if msg.status_list:
-            status_id = msg.status_list[0].status
+    # def nav_callback(self, msg):
+    #     if msg.status_list:
+    #         status_id = msg.status_list[0].status
+    #
+    #         if status_id == 0 or status_id == 3:
+    #             self.exit = 1
+    #             print "go"
+    #         else:
+    #             self.exit = 0
+    #             print "wait"
+    #     else:
+    #         self.exit = 0
+    def nav2_callback(self, msg):
+        if msg.status:
+            status_id = msg.status.status
 
-            if status_id == 0 or status_id == 3:
+            if status_id == 3:
                 self.exit = 1
+                print "go"
+            else:
+                self.exit = 0
+                print "wait"
+        else:
+            self.exit = 0
 
     def service_callback(self, req):
-        x = req.goal_position.position.x
-        y = req.goal_position.position.y
-
+        x = float(req.goal_position.position.x)
+        y = float(req.goal_position.position.y)
+        self.make_frag_pub(x, y)
         self.make_goal_pub(x,y)
 
         self.exit = 0
 
         print "Navigation .. "
         while True:
-            print "*"
+            # print self.exit
             if self.exit == 1:
                 break
         print ("finish: navigation {0:4.0f}(m) {1:4.0f}(m)".format(x,y))
